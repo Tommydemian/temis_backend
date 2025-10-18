b3ec7e5582dd6789697985c3b2799b335a41f013
Tomas Gil Amoedo <tomasgilamoedo@gmail.com>
2025-10-18 18:29:41 -0300

    feat: n multistep endpoints implemented

---
+++ config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    db_user: str
+    db_password: str
+    db_database: str
+    db_host: str = "127.0.0.1"
+    db_port: str = "5432"
+
+    SECRET_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()  # type: ignore
